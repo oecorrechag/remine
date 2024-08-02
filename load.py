1dc1b1a8a4a76ac13391396c16a8f6d604d24d65
oecorrechag <oecorrechag@unal.edu.co>
2024-08-01 23:12:11 -0500

    fc

---
+++ load.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+
+# Carga el conjunto de datos
+iris = load_iris()
+
+# Convierte los datos a un DataFrame de pandas
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+
+# Visualiza las primeras filas del DataFrame
+print('Aca se imprime el dataframe iris:')
+print('')
+print(df.head())
