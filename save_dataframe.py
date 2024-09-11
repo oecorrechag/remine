1c899c22e60d89c62266fe4ca102ebbe23f3f20b
oecorrechag <oecorrechag@unal.edu.co>
2024-09-10 20:48:49 -0500

    new try

---
+++ save_dataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Create a sample DataFrame
+data = {'column1': [1, 2, 3], 'column2': ['a', 'b', 'c']}
+df = pd.DataFrame(data)
+
+df.to_csv('data.csv', index=False)
+df.to_parquet('data.parquet', engine='pyarrow')
+df.to_excel('data.xlsx', index=False)
